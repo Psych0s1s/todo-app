f9a8790e4a67a1848de3c07beade6607eb73bdce
Maxim Fedorov <itcomtt@gmail.com>
2024-07-24 12:02:11 +0700

    added readme.md

--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,4 @@ var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE3MjE3NTY4Mzd9.6oE6dbvp4lhjq6wbOYzO_FMQ0IJb6imsj_eEPLo-w7c`
+var Token = ``
